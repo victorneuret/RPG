c4781d6a74d07d1d74d5bd3de648e7c486344060
rasorn <victor.neuret@epitech.eu>
2018-03-19 23:47:54 +0100

    - Added label text in text area

--- src/game/ui/text_area/draw_text_area.c
+++ src/game/ui/text_area/draw_text_area.c
@@ -11,7 +11,11 @@ void draw_text_area(win_t *win)
 {
 	for (text_area_t *tmp = win->game->ui->text_area; tmp;
 		tmp = tmp->next) {
+		if (tmp->game_state != win->game_state)
+			continue;
 		sfRenderWindow_drawRectangleShape(win->sf_win, tmp->box, 0);
 		sfRenderWindow_drawText(win->sf_win, tmp->text, 0);
+		if (tmp->input_text[0] == '\0')
+			sfRenderWindow_drawText(win->sf_win, tmp->label, 0);
 	}
 }
