42f12a1025b19d52aa9b021df849b1f1324eb542
HOUIS Maxime <maxime.houis@epitech.eu>
2018-03-19 12:56:26 +0100

    Weather is now framerate independant and updates according to its intensity

--- src/game/weather/weather.c
+++ src/game/weather/weather.c
@@ -8,9 +8,25 @@
 #include "render_window.h"
 #include "weather.h"
 
+static void update(win_t *win, weather_t const *weather)
+{
+	for (size_t i = 0; i < 10; i++)
+		weather->update(win);
+}
+
 void update_weather(win_t *win)
 {
+	static sfClock *timer;
+	uint32_t current_time = timer ? sfTime_asMilliseconds(
+				sfClock_getElapsedTime(timer)) : 0;
+
+	if (!timer)
+		timer = sfClock_create();
+	if (current_time < ((INTENSITY_COUNT - 1)
+				- win->game->weather_intensity) * 100.f)
+		return;
 	for (size_t i = 0; weather_list[i].update != NULL; i++)
 		if (win->game->weather_type == weather_list[i].weather_type)
-			weather_list[i].update(win);
+			update(win, &weather_list[i]);
+	sfClock_restart(timer);
 }
