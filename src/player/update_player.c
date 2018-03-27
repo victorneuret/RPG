f9b829a18bf40b7375d69636251ad2abdc664b77
rasorn <victor.neuret@epitech.eu>
2018-03-27 17:36:00 +0200

    removed useless parameter

--- src/player/update_player.c
+++ src/player/update_player.c
@@ -54,7 +54,7 @@ static void wall_on_player(player_t *player, sfVector2f *pos)
 			sfSprite_getGlobalBounds(player->sprite).width;
 }
 
-static void gravity_on_player(player_t *player, sfVector2f *pos, win_t *win)
+static void gravity_on_player(player_t *player, sfVector2f *pos)
 {
 	static double acceleration = 1.f;
 	sfVector2f bottom_pos = (sfVector2f) {0,
@@ -84,10 +84,9 @@ void update_player(win_t *win, player_t *player)
 	// 	return;
 	sfClock_restart(player->clock);
 	pos.x += X_SPEED * (win->joystick->lx / 100.f) * win->dt;
-	gravity_on_player(player, &pos, win);
+	gravity_on_player(player, &pos);
 	wall_on_player(player, &pos);
 	if (win->joystick->lx != 0)
 		update_player_direction_anim(player, win);
 	sfSprite_setPosition(player->sprite, pos);
-	printf("%f\n", win->dt);
 }
