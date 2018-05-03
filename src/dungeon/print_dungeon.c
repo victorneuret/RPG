0046ca048b76b3b6a946119ab9fb4c8c8b8c2927
rasorn <victor.neuret@epitech.eu>
2018-05-03 11:25:49 +0200

    removed printf

--- src/dungeon/print_dungeon.c
+++ src/dungeon/print_dungeon.c
@@ -8,26 +8,26 @@
 #include "dungeon.h"
 #include "str_utils.h"
 
-static void print_cells(int cell)
-{
-	if (cell == -1)
-		printf("\t");
-	else
-		printf("%d\t", cell);
-}
+// static void print_cells(int cell)
+// {
+// 	if (cell == -1)
+// 		printf("\t");
+// 	else
+// 		printf("%d\t", cell);
+// }
 
-void print_dungeon(dungeon_t *dungeon)
-{
-	for (size_t i = 0; i < (size_t) (NB_ROOMS_WIDTH * 8 + 1); i++)
-		printf("-");
-	printf("\n");
-	for (size_t i = 0; i < NB_ROOMS_HEIGHT; i++) {
-		printf("|");
-		for (size_t j = 0; j < NB_ROOMS_WIDTH; j++)
-			print_cells(dungeon->rooms[i][j]);
-		printf("|\n");
-	}
-	for (size_t i = 0; i < (size_t) (NB_ROOMS_WIDTH * 8 + 1); i++)
-		printf("-");
-	printf("\n");
-}
+// void print_dungeon(dungeon_t *dungeon)
+// {
+// 	for (size_t i = 0; i < (size_t) (NB_ROOMS_WIDTH * 8 + 1); i++)
+// 		printf("-");
+// 	printf("\n");
+// 	for (size_t i = 0; i < NB_ROOMS_HEIGHT; i++) {
+// 		printf("|");
+// 		for (size_t j = 0; j < NB_ROOMS_WIDTH; j++)
+// 			print_cells(dungeon->rooms[i][j]);
+// 		printf("|\n");
+// 	}
+// 	for (size_t i = 0; i < (size_t) (NB_ROOMS_WIDTH * 8 + 1); i++)
+// 		printf("-");
+// 	printf("\n");
+// }
