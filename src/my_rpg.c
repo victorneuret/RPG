da90d7219e062afe5697cf3f748062a5d0c5b62c
shellbear <antoine.ordonez@epitech.eu>
2018-05-03 11:06:40 +0200

    - Fixed norm errors

--- src/my_rpg.c
+++ src/my_rpg.c
@@ -42,6 +42,26 @@ static void update(win_t *win)
 	update_popups(win->game->ui->popup_list);
 }
 
+static void render_game(win_t *win)
+{
+	switch (win->game_state) {
+		case GAME:
+			draw_player(win, win->game->player);
+			break;
+		case PAUSE: break;
+		default: break;
+	}
+	draw_particles(win);
+	draw_buttons(win);
+	draw_text_hover_button(win->game->ui->hover_text_button, win);
+	draw_text_area(win);
+	render_object(win->sf_win, SPRITE,
+					win->game->ui->title_page->overlay);
+	draw_popups(win, win->game->ui->popup_list);
+	if (win->settings->display_fps)
+		draw_fps(win);
+}
+
 static void render(win_t *win)
 {
 	sfRenderWindow_clear(win->sf_win, sfBlack);
@@ -62,21 +82,9 @@ static void render(win_t *win)
 		render_object(win->sf_win, SPRITE,
 					win->game->ui->title_page->options);
 		break;
-	case GAME:
-		draw_player(win, win->game->player);
-		break;
-	case PAUSE: break;
 	default: break;
 	}
-	draw_particles(win);
-	draw_buttons(win);
-	draw_text_hover_button(win->game->ui->hover_text_button, win);
-	draw_text_area(win);
-	render_object(win->sf_win, SPRITE,
-					win->game->ui->title_page->overlay);
-	draw_popups(win, win->game->ui->popup_list);
-	if (win->settings->display_fps)
-		draw_fps(win);
+	render_game(win);
 }
 
 bool my_rpg_loop(win_t *win)
