2e37ef26e73d2c3f883934a27162f4dfd916fef1
yann <yann.sainson@epitech.eu>
2018-05-13 04:01:17 +0100

    - Added some new quest

--- include/interaction/dialog_array.h
+++ include/interaction/dialog_array.h
@@ -9,7 +9,7 @@
 
 #include <stdlib.h>
 
-static char *dialog_array[6][5] = {
+static char *dialog_array[10][5] = {
 	{
 		"Welcome to this super game!",
 		"My name is Satoru Iwata.. I will help you to progress!",
@@ -25,17 +25,40 @@ static char *dialog_array[6][5] = {
 	{
 		"Perfect! You finished the first quest!",
 		"You earn XP when you finished a quest, so" \
-		" come back to me to have\nanother one.",
+			" come back to me to have\nanother one.",
 		NULL
 	},
 	{
 		"Enemies can drop weapons, you can have 3 weapons" \
-		" in your inventory,\nchoose well.",
+			" in your inventory,\nchoose well.",
 		"Now, try to kill 10 enemies with the UZI.. Good luck!",
 		NULL
 	},
 	{
-		"WOW! You finished the game! You can now do what you want.",
+		"It is cool! You are not longer a newbie. Hmm.. so, " \
+			"I will increase\nthe difficulty.",
+		"Try to kill 5 big ghosts. Be careful, they do a lot of" \
+			" damage.",
+		NULL
+	},
+	{
+		"WOW! You are a real gamer!",
+		"I am scared by medium ghosts, can you kill 15" \
+			" of their for me please?",
+		NULL
+	},
+	{
+		"Thank you so much my friend! You save me.",
+		"That is all for now. Come back to see me later.",
+		NULL
+	},
+	{
+		"Do you already want a new quest? You do not waste your time!",
+		"OK, take or find the shotgun and kill 25 baby ghost.",
+		NULL
+	},
+	{
+		"GAME IS FINISHED!",
 		NULL
 	},
 	{
--- include/interaction/quest.h
+++ include/interaction/quest.h
@@ -43,12 +43,12 @@ typedef struct quest {
 } quest_t;
 
 typedef struct {
+	uint8_t quest_id;
 	weapon_quest_t weapon_quest;
 	enemy_quest_t enemy_quest;
 	int8_t kill;
-	uint8_t quest_id;
-	uint8_t diag_elem;
 	char **dialog;
+	uint8_t diag_elem;
 	bool state;
 } quest_declaration_t;
 
@@ -56,57 +56,94 @@ typedef struct {
 
 static const quest_declaration_t quest_declaration[] = {
 	{
+		0,
 		NONE_WEAPON_QUEST,
 		NONE_ENEMY_QUEST,
 		0,
-		0,
-		3,
 		dialog_array[0],
+		3,
 		false
 	},
 	{
+		1,
 		KILL_WEAPON_QUEST,
 		NONE_ENEMY_QUEST,
 		5,
-		1,
-		3,
 		dialog_array[1],
+		3,
 		false
 	},
 	{
+		2,
 		NONE_WEAPON_QUEST,
 		NONE_ENEMY_QUEST,
 		0,
-		2,
-		2,
 		dialog_array[2],
+		2,
 		true
 	},
 	{
+		3,
 		UZI_WEAPON_QUEST,
 		NONE_ENEMY_QUEST,
 		10,
-		3,
-		2,
 		dialog_array[3],
+		2,
 		false
 	},
 	{
-		NONE_WEAPON_QUEST,
-		NONE_ENEMY_QUEST,
-		0,
 		4,
-		1,
+		KILL_WEAPON_QUEST,
+		HEAVY_ENEMY_QUEST,
+		5,
 		dialog_array[4],
+		2,
+		false
+
+	},
+	{
+		5,
+		KILL_WEAPON_QUEST,
+		BALANCED_ENEMY_QUEST,
+		15,
+		dialog_array[5],
+		2,
 		false
 	},
 	{
+		6,
 		NONE_WEAPON_QUEST,
 		NONE_ENEMY_QUEST,
 		0,
-		3,
+		dialog_array[6],
+		2,
+		true
+	},
+	{
+		7,
+		SHOTGUN_WEAPON_QUEST,
+		LIGHT_ENEMY_QUEST,
+		25,
+		dialog_array[7],
+		2,
+		false
+	},
+	{
+		8,
+		NONE_WEAPON_QUEST,
+		NONE_ENEMY_QUEST,
+		0,
+		dialog_array[8],
+		1,
+		false
+	},
+	{
+		9,
+		NONE_WEAPON_QUEST,
+		NONE_ENEMY_QUEST,
 		0,
 		NULL,
+		0,
 		false
 	}
 };
